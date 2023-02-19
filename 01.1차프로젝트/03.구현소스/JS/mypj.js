8327158bd1e96598e4f188673a96bb1d878d99ff
dlfdydwlrshehdwk <fkrmsk147@naver.com>
2023-02-19 23:38:13 +0900

    1

--- 01.1차프로젝트/03.구현소스/JS/mypj.js
+++ 01.1차프로젝트/03.구현소스/JS/mypj.js
@@ -2,13 +2,23 @@ window.addEventListener("DOMContentLoaded",()=>{
 const qs = x =>document.querySelector(x);
 
     console.log("로딩완료");
-
+// 메인페이지 버튼
 btn = qs(".btn");
+// 사이트맵
 sitemap = qs(".sitemap");
+// 사이트맵 버튼
+stb = qs(".sitemap_btn");
 
+// 메인페이지 버튼누를시 사이트맵에 클래스 on 추가
 btn.onclick = ()=>{
     // console.log(btn);
     sitemap.classList.add("on");
 };
+
+// 사이트맵에 버튼클릭시 사이트맵에 클래스 on 빼기
+stb.onclick=()=>{
+    sitemap.classList.remove("on");
+}
+
 
 });//로드구역 ///
