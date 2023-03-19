cccd6372004665f8d9ada6e3220b920775682f71
dlfdydwlrshehdwk <fkrmsk147@naver.com>
2023-03-20 04:12:23 +0900

    235

--- 01.1차프로젝트/03.구현소스/JS/mypj.js
+++ 01.1차프로젝트/03.구현소스/JS/mypj.js
@@ -204,13 +204,177 @@ p5h1txt();
 
 // 페이지 2 인터벌
 
-// 대상 .bx3 ul
-const p2slide = qs('.bx3slide')
-console.log(p2slide)
+// 대상 .bx3 ul 슬라이드박스
+const p2slide = qs('.bx3slide');
+// 대상 슬라이드의 li들
+const p2slist = qsa('.bx3slide li');
+// 대상 페이지 2의 버튼들
+const p2btns = qsa('.p2btns div');
+console.log(p2btns)
 // 기능 : 일정시간마다 왼쪽으로 -39%씩이동
 // 화살표를 누르면 맨끝사진을 다른맨끝으로 잘라서 넣은 후 방향에 맞게 이동
 
+// 광클금지변수 : 0 - 허용, 1 - 불허용
+let qwer = 0;
 
+// 2. 슬라이드 변경함수 만들기
+// 호출시 seq에 들어오는 값중 1은 오른쪽, 0은 왼쪽
+const goSlide = (seq) => {
+    //  console.log("슬고우!", seq);
+
+    //  console.log("못들어갔어!!!!");
+
+    // 광클금지 설정하기 //////
+    if (qwer) return;
+    qwer = 1; // 잠금!
+    setTimeout(() => {
+        qwer = 0; // 해제!
+    }, 400); /// 0.4초후 해제! ///
+
+    //  console.log("나,들어왔어!!!");
+
+    // 0. 현재의 슬라이드 li수집하기
+    let clist = p2slide.querySelectorAll("li");
+    // clist -> current list 현재 리스트
+
+    // 1. 방향에 따른 분기
+    // 1-1. 오른쪽버튼 클릭시 ////////////////
+    if (seq) {
+         console.log("오른!");
+
+        // 1. 슬라이드 이동전 먼저 잘라낸다!
+        // 이유: 슬라이드 순서를 왼쪽이동과 동일하게 함!
+        // 중앙확대 트랜지션 적용시 동작이 달라지므로!
+
+        // (1-1) 바깥에 나가있는 첫번째 슬라이드
+        //       li를 잘라서 맨뒤로 보낸다!
+        p2slide.appendChild(clist[0]);
+        // (1-2) 동시에 left값을 -110%으로 변경한다!
+        p2slide.style.left = "-39%";
+        // (1-3) 트랜지션 없애기!
+        p2slide.style.transition = "none";
+
+        
+        // (2) 오른쪽 버튼 클릭시 다음 슬라이드가
+        //     나타나도록 슬라이드 박스의 left값을
+        //     -220%로 변경시킨다.
+
+        // [코드분리하기!] //////////////////////////
+        // -> 같은속성변경을 같은 메모리공간에서 수행하면
+        // 변경효과가 없음!!! 
+        setTimeout(() => {
+            p2slide.style.left = "-78%";
+            p2slide.style.transition = "left .4s ease-in-out";                
+        }, 1); //// 타임아웃 //////
+        // 시간에 0을쓰면 인터발호출시 트랜지션이 안먹히는 에러가 있음
+        // 1만써도 괜찮음~
+
+        // -> 타이밍함수는 기존 함수인 스택(Stack)메모리 공간이 아닌
+        // 대기실행 공간인 큐(Queue)메모리공간에서 실행하므로
+        // 코드가 동시에 바뀌는 것을 막아주고 의도한 대로
+        // 시차실행을 가능하게 해준다!
+
+
+    } //////////// if : 오른쪽클릭시 //////
+
+    // 1-2. 왼쪽버튼 클릭시 //////////////
+    else {
+         console.log("왼쪽!");
+
+        // (1) 왼쪽버튼 클릭시 이전 슬라이드가
+        // 나타나도록 하기위해 우선 맨뒤 li를
+        // 맨앞으로 이동한다.
+        // slide.insertBefore(넣을놈,넣을놈전놈)
+        // slide.insertBefore(맨끝li,맨앞li)
+        p2slide.insertBefore(clist[clist.length - 1], clist[0]);
+
+        // (2) 동시에 left값을 -117%로 변경한다.
+        p2slide.style.left = "-117%";
+        // 이때 트랜지션을 없앤다(한번실행후 부터 생기므로!)
+        p2slide.style.transition = "none";
+
+        // (3) 그 후 left값을 -220%으로 애니메이션하여
+        // 슬라이드가 왼쪽에서 들어온다.
+        // 동일 속성인 left가 같은 코딩처리 공간에 동시에
+        // 있으므로 이것을 분리해야 효과가 있다!
+        // setTimeout을 사용한다!
+        setTimeout(() => {
+            p2slide.style.left = "-78%";
+            p2slide.style.transition = "left .4s ease-in-out";
+        }, 0); ////// 타임아웃 /////////
+
+    } //////////// else : 왼쪽클릭시 //////
+
+    // 2. 현재 슬라이드 순번과 같은 블릿표시하기
+    // 대상: .indic li -> indic변수
+    // 2-1. 현재 배너리스트 업데이트하기
+    clist = p2slide.querySelectorAll("li");
+    // !!!!! 오른쪽이든 왼쪽이든 먼저 잘라내기 때문에 
+    // 순번은 3번째로 일치함!!!!!!
+    // console.log("다시수집:",clist);
+
+    // 2-2.방향별 읽어올 슬라이드 순번으로 "data-seq"값 읽어오기
+    // 세번째 슬라이드가 주인공이니까 0,1,2 즉 2번을 쓰면됨!!!
+    let cseq = clist[2].getAttribute("data-seq");
+    //  console.log("현재순번:", cseq);
+
+    // 2-3. 블릿초기화
+    // for (let x of indic) x.classList.remove("on");
+
+    // 2-4. 읽어온 슬라이드 순번의 블릿에 클래스 "on"넣기
+    // indic[cseq].classList.add("on");
+}; ////////// goSlide함수 ///////////
+
+// 3. 이동버튼대상에 이벤트 설정하기
+p2btns.forEach((ele, idx) => {
+    ele.onclick = () => {
+        // 0. 기본이동막기
+        event.preventDefault();
+        
+        // 1. 인터발지우기함수 호출!
+        clearAuto();
+        // 2. 슬라이드 함수 호출!
+        goSlide(idx);
+    }; ///// click함수 //////
+}); /////// forEach //////////
+
+// 인터발함수 지우기위한 변수
+let autoI;
+// 타임아웃함수 지우기위한 변수
+let autoT;
+
+/************************************ 
+    함수명: autoSlide
+    기능: 인터발함수로 슬라이드함수 호출
+************************************/
+function autoSlide(){
+    console.log("인터발시작!");
+    // 인터발함수로 슬라이드함수 호출하기
+    autoI = setInterval(()=>goSlide(1),3000);
+} ////////////// autoSlide함수 //////////
+
+// 자동넘김 최초호출!
+autoSlide();
+
+/************************************ 
+    함수명: clearAuto
+    기능: 인터발함수를 지우고 다시셋팅
+************************************/
+function clearAuto(){
+console.log("인터발멈춤!");
+// 1. 인터발 지우기
+clearInterval(autoI);
+
+// 2. 타임아웃도 지우지 않으면
+// 쌓여서 타임아웃 쓰나미실행이 발생한다!
+clearTimeout(autoT);
+
+// 3. 잠시후 다시 작동하도록 타임아웃으로
+// 인터발함수를 호출한다! 
+// 5초후(인터발은 3초후, 토탈 8초후 작동시작)
+autoT = setTimeout(autoSlide,5000);
+
+} ///////// clearAuto 함수 /////////////
 
 
 
