b1c4862270294a02b53daf51c731f4df2a830df0
dlfdydwlrshehdwk <118067728+dlfdydwlrshehdwk@users.noreply.github.com>
2023-06-09 09:35:12 +0900

    1

--- 03.SPA프로젝트/03.구현소스/third/src/Fila/modules/Bgbx.js
+++ 03.SPA프로젝트/03.구현소스/third/src/Fila/modules/Bgbx.js
@@ -1,15 +1,23 @@
 // Bgbx 컴포넌트 모듈
 import '../css/bgbx.css';
 import bgsrc from '../Bg';
+import React from 'react';
 
 function Bgbx(props){
     return(
         <>
-            <div className="bgbx" 
-            style={{backgroundImage:'url('+ bgsrc[props.sub] +')'}}
-            >
-                <div className="tit">
-                    {props.title}
+            <div className="bgbxwrap">
+                <div className="bagbx">
+                    <div className="tit">
+                        {props.title}
+                    </div>
+                    <div className="bag20bx">
+                        <img className="bgbx" src={bgsrc[props.sub]} />
+                        <div className='vidwrap'>
+                            
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </>
